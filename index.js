885f7170739101ca7e911add5cce9152c8a3fed2
kurnia22 <kurniawati.22.nk@gmail.com>
2017-05-06 21:36:06 +0700

    add template

--- index.js
+++ index.js
@@ -5,47 +5,59 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var bodyParser = require('body-parser')
+var exphbs = require("express-handlebars")
 
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs')
 
 
 //=============routing===========
 
 app.route('/')
-	.get(function(req, res){
-		res.sendFile(path.join(__dirname + '/view/home.html'))
+    .get(function(req, res) {
+        res.render('home');
 
-	})
-	.post(function(req, res){
-		var angka_1 = req.body.angka1
-        var angka_2 = req.body.angka2
+    })
 
-        if (angka_1 == "" && angka_2 == "") {
-            console.log("error masukkan angka 1 dan angka 2")
-            res.send("error masukkan angka 1 dan angka 2")
-        } else if (angka_1 == "") {
-            console.log("error masukkan angka 1")
-            res.send("error masukkan angka 1")
-        } else if (angka_2 == "") {
-            console.log("error masukkan angka 2")
-            res.send("error masukkan angka_2")
-        } else {
-            penjumlahan(angka_1, angka_2)
-        }
+.post(function(req, res) {
+    var angka_1 = req.body.angka1
+    var angka_2 = req.body.angka2
 
-        function penjumlahan(a, b) {
-            var total = parseInt(a) + parseInt(b)
-            res.send("total" + a + "+" + b + "=" + total)
+    if (angka_1 == "" && angka_2 == "") {
+        console.log("error masukkan angka 1 dan angka 2")
+        res.send("error masukkan angka 1 dan angka 2")
+    } else if (angka_1 == "") {
+        console.log("error masukkan angka 1")
+        res.send("error masukkan angka 1")
+    } else if (angka_2 == "") {
+        console.log("error masukkan angka 2")
+        res.send("error masukkan angka_2")
+    } else {
+        penjumlahan(angka_1, angka_2)
+    }
 
-        }
+    function penjumlahan(a, b) {
+        var total = parseInt(a) + parseInt(b)
+        // res.send("total" + a + "+" + b + "=" + total)
 
-	})
+        res.render("hasil", {
+            judul: "hasil penjumlahan",
+            hasil_penjumlahan: total
 
-	//===============webserver======//
+        })
 
-	app.listen(4000,function(){
-		console.log('ini contoh web server sederhana dengan node js')
-	})
+
+    }
+
+})
+
+//===============webserver======//
+
+app.listen(4000, function() {
+    console.log('ini contoh web server sederhana dengan node js')
+})
