b1e94ccb6bce91e348669ec72f11f662729fd131
kurnia22 <kurniawati.22.nk@gmail.com>
2017-05-07 21:35:21 +0700

    make query in backend & fullstack

--- index.js
+++ index.js
@@ -15,30 +15,101 @@ app.use(bodyParser.json());
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs')
 
+//============SET UP=============
+const Pool = require('pg').Pool;
+var config = {
+    user: 'nia',
+    database: 'resep',
+    password: 'nia',
+    host: '192.168.100.6',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000,
+};
+process.on('unhandledRejection', function(e) {
+    console.log(e.message, e.stack)
+})
+var pool = new Pool(config)
+
 
 //=============routing===========
 
 app.route('/')
     .get(function(req, res) {
-        res.render('resep');
+        // Query
+        pool.query('SELECT * from resep')
+            .then((result) => {
+                var hasil = result.rows
+                console.log('number:', hasil);
 
+                res.render('resep', {
+                    data: hasil,
+                    judul: 'Resep App with NodeJS'
+                })
+
+            })
+            .catch((err) => {
+                console.error('error running query', err);
+            });
     })
 
-.post(function(req, res) {
-  var nama_resep = req.body.nama_resep
-  var Deskripsi = req.body.deskripsi
-  var Penulis = req.body.penulis
-  var cara_pembuatan = req.body.cara_pembuatan
+    .post(function(req, res) {
+        var id = req.body.id
+        var nama_resep = req.body.nama_resep
+        var Deskripsi = req.body.deskripsi
+        var Penulis = req.body.penulis
+        var cara_pembuatan = req.body.cara_pembuatan
 
-  console.log(nama_resep)
-  console.log(Deskripsi)
-  console.log(Penulis)
-  console.log(cara_pembuatan)
+        console.log(nama_resep+' '+Deskripsi+' '+Penulis+' '+cara_pembuatan)
+        
+        var query_post = 'insert into resep(id, nama_resep, Deskripsi, Penulis, cara_pembuatan)' + 
+                            'values($1, $2, $3, $4, $5)'
 
-})
+        pool.query(query_post, [id, nama_resep, Deskripsi, Penulis, cara_pembuatan])
+            .then((result)=>{
+                console.log('Success insert data:',result);
+                res.redirect('/')
+            })
+            .catch((err)=>{
+                console.error('error running query', err);
+            })
+    })
+
+    app.route('/detail_resep')
+        .get(function(req, res){
+            var ID_Resep = req.query.id
+            console.log('ID: ', ID_Resep)
+
+            // 1. Query ID yang diinginkan
+            // 2. hasil query di passing render ke frontend
+            var query_getSingleData = 'select * from resep where id = $1'
+            pool.query(query_getSingleData, [ID_Resep])
+                .then((result)=>{
+                    console.log('Success get single data:', result.rows[0]);
+                    res.render('detail_resep',{
+                        ID: result.rows[0].id,
+                        Nama_Resep: result.rows[0].nama_resep,
+                        Deskripsi: result.rows[0].deskripsi,
+                        Penulis: result.rows[0].penulis,
+                        Cara_Pembuatan: result.rows[0].cara_pembuatan,
+                    })
+                })
+                .catch((err)=>{
+                    console.error('error running query', err);
+                });
+        })
+        .post(function(req, res){
+
+        })
+
+
 
 //===============webserver======//
 
-app.listen(4000, function() {
-    console.log('ini contoh web server sederhana dengan node js')
-})
+pool
+    .query('CREATE TABLE IF NOT EXISTS resep(id SERIAL PRIMARY KEY, nama_resep VARCHAR(40) not null, deskripsi VARCHAR(40) not null, penulis VARCHAR(40) not null, cara_pembuatan VARCHAR(240) not null)')
+    .then(function() {
+        app.listen(4000, function() {
+            console.log('server is listening on 4000')
+        })
+    })
